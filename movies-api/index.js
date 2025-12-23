54994f035d87bf6b0ba2c34a1abf6e1701959662
KyleG4 <20107489@setu.ie>
2025-12-23 16:16:04 +0000

    Added start files for CA2

---
+++ movies-api/index.js
@@ -0,0 +1,31 @@
+import authenticate from './authenticate';
+import './db';
+import dotenv from 'dotenv';
+import express from 'express';
+
+import usersRouter from './api/users';
+import cors from 'cors';
+
+dotenv.config();
+
+const errHandler = (err, req, res, next) => {
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here is the details: ${err.stack} `);
+};
+
+const app = express();
+const port = process.env.PORT;
+
+// Enable CORS for all requests
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/users', usersRouter);
+app.use(errHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
---
+++ src/pages/topRatedPage.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "../components/spinner";
+import PageTemplate from "../components/templateMovieListPage";
+import { getTopRatedMovies } from "../api/tmdb-api";
+import AddToFavoritesIcon from "../components/cardIcons/addToFavorites";
+import AddToWatchlist from "../components/cardIcons/addToWatchlist";
+
+const TopRatedPage = () => {
+  const { data, error, isPending, isError } = useQuery({
+    queryKey: ['topRated'],
+    queryFn: getTopRatedMovies,
+  });
+
+  if (isPending) return <Spinner />;
+  if (isError) return <h1>{error.message}</h1>;
+
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Top Rated Movies"
+      movies={movies}
+      action={(movie) => (
+        <>
+          <AddToFavoritesIcon movie={movie} />
+          <AddToWatchlist movie={movie} />
+        </>
+      )}
+    />
+  );
+};
+
+export default TopRatedPage;
