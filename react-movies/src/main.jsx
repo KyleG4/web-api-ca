c408362e7c24cc455cafb1db07498d5d1bf0feb8
KyleG4 <20107489@setu.ie>
2025-12-29 19:05:54 +0000

    Wire up AuthContext and add Login/Signup routes

--- react-movies/src/main.jsx
+++ react-movies/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Navigate, Routes } from "react-router";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom"; // Changed to react-router-dom
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -8,9 +8,14 @@ import HomePage from "./pages/homePage";
 import MovieDetailsPage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import AddMovieReviewPage from "./pages/addMovieReviewPage";
-import MovieReviewPage from "./pages/movieReviewPage"; 
+import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from "./components/siteHeader";
 import MoviesContextProvider from "./contexts/moviesContext";
+
+// 1. IMPORT AUTH CONTEXT AND PAGES
+import AuthContextProvider from "./contexts/authContext";
+import LoginPage from "./pages/loginPage";
+import SignUpPage from "./pages/signUpPage";
 
 import NowPlayingPage from "./pages/nowPlayingPage";
 import TopRatedPage from "./pages/topRatedPage";
@@ -32,25 +37,30 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <SiteHeader />
-        <MoviesContextProvider>
-          <Routes>
-            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
-            <Route path="/reviews/:id" element={<MovieReviewPage />} /> 
-            <Route path="/movies/:id" element={<MovieDetailsPage />} />
+        <AuthContextProvider> {/* 2. WRAP THE APP IN AUTH CONTEXT */}
+          <SiteHeader />
+          <MoviesContextProvider>
+            <Routes>
+              {/* 3. ADD THE PUBLIC AUTH ROUTES */}
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
 
-            
-            <Route path="/now-playing" element={<NowPlayingPage />} />
-            <Route path="/top-rated" element={<TopRatedPage />} />
-            <Route path="/upcoming" element={<UpcomingPage />} />
-            <Route path="/movies/:id/recommendations" element={<RecommendationsPage />} />
-            <Route path="/watchlist" element={<WatchlistPage />} />
+              <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+              <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+              <Route path="/reviews/:id" element={<MovieReviewPage />} />
+              <Route path="/movies/:id" element={<MovieDetailsPage />} />
+              
+              <Route path="/now-playing" element={<NowPlayingPage />} />
+              <Route path="/top-rated" element={<TopRatedPage />} />
+              <Route path="/upcoming" element={<UpcomingPage />} />
+              <Route path="/movies/:id/recommendations" element={<RecommendationsPage />} />
+              <Route path="/watchlist" element={<WatchlistPage />} />
 
-            <Route path="/" element={<HomePage />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </MoviesContextProvider>
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </MoviesContextProvider>
+        </AuthContextProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
