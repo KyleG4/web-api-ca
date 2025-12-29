dd0ea4198b20af72d4090ed56edcde8b7dd2d601
KyleG4 <20107489@setu.ie>
2025-12-29 18:57:51 +0000

    Connect Reviews Router to main server

--- movies-api/index.js
+++ movies-api/index.js
@@ -3,8 +3,8 @@ import './db';
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
-
 import usersRouter from './api/users';
+import reviewsRouter from './api/reviews'; // <--- ADD THIS
 import cors from 'cors';
 
 dotenv.config();
@@ -19,13 +19,12 @@ const errHandler = (err, req, res, next) => {
 const app = express();
 const port = process.env.PORT;
 
-// Enable CORS for all requests
 app.use(cors());
-
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter);
+app.use('/api/reviews', reviewsRouter); // <--- ADD THIS
 app.use(errHandler);
 
 app.listen(port, () => {
